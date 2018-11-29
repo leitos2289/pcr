115e139a1d636554260c5b7049907c7881260a4e
victor <victor.vinanzacap@gmail.com>
2018-11-29 17:38:02 -0500

    Modifica Models

--- PRC/mesas/models.py
+++ PRC/mesas/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 class Persona(models.Model):
     idpersona = models.AutoField(db_column='IDPERSONA', primary_key=True)
+    cedula = models.CharField(db_column='CEDULA', max_length=10)
     nombre = models.CharField(db_column='NOMBRE', max_length=100)
